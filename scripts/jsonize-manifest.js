ebc022d84fd8bb655765ff8ba8659b5b2e18ca79
Keiichiro Ui <keiichiro.ui@gmail.com>
2023-11-03 15:36:30 +0900

    Make the type specification robust.

--- scripts/jsonize-manifest.js
+++ scripts/jsonize-manifest.js
@@ -1,7 +1,9 @@
 import manifest from "../src/manifest.js";
 import { readFileSync } from "node:fs";
 import path from "node:path";
+import { fileURLToPath } from 'node:url';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const pkg = JSON.parse(
   readFileSync(path.join(__dirname, "..", "package.json"), "utf8"),
 );
